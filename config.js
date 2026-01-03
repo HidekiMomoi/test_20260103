ae2e7b3792b707999b2147836252e25b8f7c34a1
HidekiMomoi <139665285+HidekiMomoi@users.noreply.github.com>
2026-01-03 21:30:13 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBa1uHGYTRft3O0nGiCSTdOCnjnuWHD-R8",
+  authDomain: "task-app-72b7d.firebaseapp.com",
+  projectId: "task-app-72b7d",
+  storageBucket: "task-app-72b7d.firebasestorage.app",
+  messagingSenderId: "677238706690",
+  appId: "1:677238706690:web:f8fe3af7bd3efcb9900d1b"
+};
