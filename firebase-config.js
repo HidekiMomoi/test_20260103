1dfeaebaa62be58460eac650d13a19828298b3d6
HidekiMomoi <139665285+HidekiMomoi@users.noreply.github.com>
2026-01-03 20:28:13 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "ここ",
+  authDomain: "ここ",
+  projectId: "ここ",
+  storageBucket: "ここ",
+  messagingSenderId: "ここ",
+  appId: "ここ"
+};
