862ff5c582c8ea4f7cd8f268423db146933770af
HidekiMomoi <139665285+HidekiMomoi@users.noreply.github.com>
2026-01-03 20:29:30 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // firebase-config.js
 export const firebaseConfig = {
-  apiKey: "ここ",
-  authDomain: "ここ",
-  projectId: "ここ",
-  storageBucket: "ここ",
-  messagingSenderId: "ここ",
-  appId: "ここ"
+      apiKey: "AIzaSyBa1uHGYTRft3O0nGiCSTdOCnjnuWHD-R8",
+      authDomain: "task-app-72b7d.firebaseapp.com",
+      projectId: "task-app-72b7d",
+      storageBucket: "task-app-72b7d.firebasestorage.app",
+      messagingSenderId: "677238706690",
+      appId: "1:677238706690:web:f8fe3af7bd3efcb9900d1b"
 };
